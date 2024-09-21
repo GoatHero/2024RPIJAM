abd392f95f7ba691cf0053b78020aa74ca11e9f0
Ben Herman <bherman24@dtechhs.org>
2024-09-21 16:33:17 -0400

    ok

--- 2024rpijam/scripts/enemy/snake/SnakeLink.cs
+++ 2024rpijam/scripts/enemy/snake/SnakeLink.cs
@@ -12,12 +12,14 @@ public partial class SnakeLink : BaseRigidBodyEnemy
 	public override void _PhysicsProcess(double delta) {
 		base._PhysicsProcess(delta);
 
+		float dt = (float)delta;
+
 		Vector2 vel = LinearVelocity.Rotated(-GlobalRotation);
 		vel.X = 0;
 		if (vel.Length() > 0) {
 			vel.Y *= -0.05f;
 			vel.Rotated(GlobalRotation);
-			ApplyImpulse(vel);
+			// ApplyImpulse(dt*vel);
 		}
 		
 	}
