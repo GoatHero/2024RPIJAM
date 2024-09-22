84f027f4f0c71ff938208826e312a07f8b477d13
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-09-22 17:19:32 -0400

    level 4 work

--- 2024rpijam/scripts/environment/Narrator.cs
+++ 2024rpijam/scripts/environment/Narrator.cs
@@ -1,94 +1,93 @@
-using System;
-using Godot.Collections;
 using Godot;
+using Godot.Collections;
+using System;
 
 public partial class Narrator : BaseCharacterBodyEnemy {
-	[Export]
-	public Array<Area2D> areas { get; set; } = new();
-	[Export]
-	public Array<string> lines { get; set; } = new();
-	[Export]
-	public Array<float> times { get; set; } = new();
+    [Export]
+    public Array<Area2D> areas { get; set; } = new();
+    [Export]
+    public Array<string> lines { get; set; } = new();
+    [Export]
+    public Array<float> times { get; set; } = new();
 
-	[Export]
-	public bool isStatic = false; 
-	public AnimatedSprite2D animatedSprite2D;
-	public Timer lineTimer;
+    [Export]
+    public bool isStatic = false;
+    public AnimatedSprite2D animatedSprite2D;
+    public Timer lineTimer;
 
-	public override void _Ready() {
-		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		animatedSprite2D.Stop();
-		lineTimer = GetNode<Timer>("Timer");
-		for (int i = 0; i < areas.Count; i++)
-		{
-			areas[i].BodyEntered += (Node2D node) => {
-				if (node is Player)
-					say(lines[i], times[i]);
-			};
-		}
-	}
+    public override void _Ready() {
+        animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        animatedSprite2D.Stop();
+        lineTimer = GetNode<Timer>("Timer");
+        for(int i = 0; i < areas.Count; i++) {
+            areas[i].BodyEntered += (Node2D node) => {
+                if(node is Player)
+                    say(lines[i], times[i]);
+            };
+        }
+    }
 
-	public override void _PhysicsProcess(double delta) {
-		if (!isStatic) {
-			Vector2 dif = GlobalPosition - player.GlobalPosition;
-			Vector2 wantedDif = dif/dif.Length()*500;
-			if (Math.Abs(wantedDif.X) < Math.Abs(wantedDif.Y)) {
-				(wantedDif.X, wantedDif.Y) = (Math.Abs(wantedDif.Y) * Math.Sign(wantedDif.X), Math.Abs(wantedDif.X) * Math.Sign(wantedDif.Y));
-			}
-			moveToPosition(player.GlobalPosition + wantedDif);
-		}
-		if (!animatedSprite2D.IsPlaying()) {
-			animatedSprite2D.Play();
-		}
-	}
+    public override void _PhysicsProcess(double delta) {
+        if(!isStatic) {
+            Vector2 dif = GlobalPosition - player.GlobalPosition;
+            Vector2 wantedDif = dif / dif.Length() * 500;
+            if(Math.Abs(wantedDif.X) < Math.Abs(wantedDif.Y)) {
+                (wantedDif.X, wantedDif.Y) = (Math.Abs(wantedDif.Y) * Math.Sign(wantedDif.X), Math.Abs(wantedDif.X) * Math.Sign(wantedDif.Y));
+            }
+            moveToPosition(player.GlobalPosition + wantedDif);
+        }
+        if(!animatedSprite2D.IsPlaying()) {
+            animatedSprite2D.Play();
+        }
+    }
 
-	public void setDialogue(int line) {
-		string lines = string.Empty;
-		switch(line) {
-			case 0:
-				break;
-			case 1:
-				lines = "You barely interacted with it kill every enemy this time";
-				break;
-			case 2:
-				lines = "You violent brute your really did it\n you really killed everything\n you cant be trusted with a weapon you monster";
-				break;
-			case 3:
-				lines = "I think I gave you to much\n this game is meant for a normal person not someone freaky like you";
-				break;
-			case 4:
-				break;
-			case 5:
-				lines = "NO NO NO\n I WILL NOY LET YOU LEAVE\n";
-				break;
-			case 6:
-				lines = "Ouch";
-				timer.Start();
-				break;
-			case 7:
-				lines = "KILL THEM";
-				timer.Start();
-				break;
-			case 8:
-				lines = "HAHAHAHA";
-				timer.Start();
-				break;
-			default:
-				lines = string.Empty;
-				break;
-		}
-		say(lines);
-	}
+    public void setDialogue(int line) {
+        string lines = string.Empty;
+        switch(line) {
+            case 0:
+                break;
+            case 1:
+                lines = "You barely interacted with it kill every enemy this time";
+                break;
+            case 2:
+                lines = "You violent brute your really did it\n you really killed everything\n you cant be trusted with a weapon you monster";
+                break;
+            case 3:
+                lines = "I think I gave you to much\n this game is meant for a normal person not someone freaky like you";
+                break;
+            case 4:
+                break;
+            case 5:
+                lines = "NO NO NO\n I WILL NOT LET YOU LEAVE\n";
+                break;
+            case 6:
+                lines = "Ouch";
+                timer.Start();
+                break;
+            case 7:
+                lines = "KILL THEM";
+                timer.Start();
+                break;
+            case 8:
+                lines = "HAHAHAHA";
+                timer.Start();
+                break;
+            default:
+                lines = string.Empty;
+                break;
+        }
+        say(lines);
+    }
 
 
-	public void say(string str, float time = -1) {
-		GetNode<Label>("AnimatedSprite2D/Label").Text = str;
-		lineTimer.Start(time);
-		animatedSprite2D.Play();
-	}
+    public void say(string str, float time = -1) {
+        GetNode<Label>("AnimatedSprite2D/Label").Text = str;
+        lineTimer.Start(time);
+        animatedSprite2D.Play();
+    }
 
     public void clearText() {
         setDialogue(-1);
-		animatedSprite2D.Stop();
+        animatedSprite2D.Stop();
     }
 }
