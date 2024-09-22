b7f44f874506b1c3f3e7d9fa0bb1cc5507df1c92
EggyMath <55564321+EggyMath@users.noreply.github.com>
2024-09-22 15:32:34 -0400

    smth

--- 2024rpijam/scripts/UI/LevelSelect.cs
+++ 2024rpijam/scripts/UI/LevelSelect.cs
@@ -5,34 +5,20 @@ using System;
 public partial class LevelSelect : Control
 {
 	[Export]
-	public int highestLevel = 1;
+	public int highestLevel = 3;
 	[Export]
 	public int levelCount = 4;
-	public Godot.Collections.Array<string> buttons = new Godot.Collections.Array<string>{
-		"level1",
-		"level2",
-		"level3",
-		"level4"
-	};
 	
 	public override void _Ready(){
-		GD.Print("Start");
-		for(int i = 1; i < buttons.Count + 1; i++){
+		for(int i = 1; i < levelCount + 1; i++){
 			string getter = "Level" + i;
+			GD.Print("Start: " + getter);
 			Button button = GetNode<Button>(getter);
 			if(i > highestLevel)
 				button.Disabled = true;
 			else
 				button.Disabled = false;
+			GD.Print("End");
 		}
-		GD.Print("End");
-		/*base._Ready();
-		foreach(var child in GetChildren()){
-			GD.Print("Child: " + child);
-			if(IsInstanceValid(child) && child is BaseButton button){
-				GD.Print("found");
-			}
-		}
-		*/
 	}
 }
