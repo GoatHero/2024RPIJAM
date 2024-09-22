9bae4e6a8a0172ba12553db0a50beba724cee8b0
Ben Herman <bherman24@dtechhs.org>
2024-09-22 14:21:07 -0400

    fixed

--- 2024rpijam/scripts/UI/SceneChangeButton.cs
+++ 2024rpijam/scripts/UI/SceneChangeButton.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class SceneChangeButton : Control
+public partial class SceneChangeButton : Button
 {
 	//When called by button makes a copy of original untocuhed script and uses that
 	//And so you need to pass in scene info through button
