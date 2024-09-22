73a9ea32df21937adc22b4da0121851714997143
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-09-22 10:54:20 -0400

    Laser is done

--- 2024rpijam/scenes/enemy/Laser.cs
+++ 2024rpijam/scenes/enemy/Laser.cs
@@ -2,30 +2,23 @@ using Godot;
 public partial class Laser : Node2D {
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
-        GetTree().PhysicsFrame += setUp;
     }
 
     public void setUp() {
-        var spaceState = GetWorld2D().DirectSpaceState;
-        // use global coordinates, not local to node
-        var query = PhysicsRayQueryParameters2D.Create(Vector2.Zero, new Vector2(50, 100));
 
-        query.Exclude.Add(GetNode<StaticBody2D>("StaticBody2D").GetRid());
-        Godot.Collections.Dictionary result = spaceState.IntersectRay(query);
+        Vector2 pos = GetNode<RayCast2D>("RayCast2D").GetCollisionPoint();
 
-        if(result.Count != 0) {
-            Vector2 pos = (Vector2)result["position"];
-            pos = ToLocal(pos);
-            Line2D line = GetNode<Line2D>("Line2D");
-            line.Points[1] = pos;
-            line.GetChild<Node2D>(0).Position = pos;
-            CollisionShape2D laserCollider = GetNode<CollisionShape2D>("Area2D/CollisionShape2D");
-            ((RectangleShape2D)laserCollider.Shape).Size = new Vector2(Mathf.Abs(line.Points[0].X - pos.X), ((RectangleShape2D)laserCollider.Shape).Size.Y);
-            laserCollider.Position = line.Points[0] + line.Points[0] - pos;
-        } else {
-            GD.PrintErr("No Object for laser to Collide with");
-        }
-        GetTree().PhysicsFrame -= setUp;
+        GD.Print(pos);
+        pos = ToLocal(pos);
+        GD.Print(pos);
+        Line2D line = GetNode<Line2D>("Line2D");
+        GD.Print(line.Points[0]);
+        line.SetPointPosition(0, pos);
+        line.GetNode<GpuParticles2D>("GPUParticles2D").Position = pos;
+        CollisionShape2D laserCollider = GetNode<CollisionShape2D>("Area2D/CollisionShape2D");
+        ((RectangleShape2D)laserCollider.Shape).Size = new Vector2(Mathf.Abs(line.Points[1].X - pos.X), ((RectangleShape2D)laserCollider.Shape).Size.Y);
+        laserCollider.Position = (line.Points[0] + line.Points[1]) / 2;
+
     }
 
     public void hit(Node2D node) {
@@ -40,6 +33,7 @@ public partial class Laser : Node2D {
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) {
+    public override void _PhysicsProcess(double delta) {
+        setUp();
     }
 }
