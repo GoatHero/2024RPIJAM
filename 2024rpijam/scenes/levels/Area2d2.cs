0d6003c4a82dc4600f4a10238cee4786d4e9301c
Ben Herman <bherman24@dtechhs.org>
2024-09-22 17:59:39 -0400

    fixed level stuff

---
+++ 2024rpijam/scenes/levels/Area2d2.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class Area2d2 : Area2D
+{
+	public override void _Process(double delta)
+	{
+		if (!HasOverlappingBodies()) {
+			GlobalData g = (GlobalData)(GetTree().Root.GetNode("GlobalData"));
+			g.completedLevel();
+			GetTree().ChangeSceneToPacked(ResourceLoader.Load<PackedScene>("res://scenes/UI/LevelSelect.tscn"));
+		}
+	}
+}
