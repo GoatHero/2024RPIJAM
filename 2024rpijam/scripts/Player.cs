5a3eb7e2fb4bc81471f3d19ac5b79fe5c2cfee8a
EggyMath <55564321+EggyMath@users.noreply.github.com>
2024-09-22 14:16:28 -0400

    ButtonNoWorky

--- 2024rpijam/scripts/Player.cs
+++ 2024rpijam/scripts/Player.cs
@@ -2,186 +2,186 @@ using Godot;
 
 public partial class Player : CharacterBody2D {
 
-    [Export]
-    public float fallMulti = 1.5f;
-    [Export]
-    public float dashSpeed = 4800.0f;
-    [Export]
-    public float speed = 2800.0f;
-    [Export]
-    public float airSpeed = 1000.0f;
-    [Export]
-    public float jumpVelocity = -600.0f;
-    [Export]
-    public float wallJumpVelocity = 750.0f;
-    [Export]
-    public float airPoundMulti = 3f;
+	[Export]
+	public float fallMulti = 1.5f;
+	[Export]
+	public float dashSpeed = 4800.0f;
+	[Export]
+	public float speed = 2800.0f;
+	[Export]
+	public float airSpeed = 1000.0f;
+	[Export]
+	public float jumpVelocity = -600.0f;
+	[Export]
+	public float wallJumpVelocity = 750.0f;
+	[Export]
+	public float airPoundMulti = 3f;
 
-    [Export]
-    public float health = 100;
-    [Export]
-    public float maxHealth = 100;
-    [Export]
-    public float attackDamage = 5f;
-    [Export]
-    public float attackCooldown = 0.6f;
+	[Export]
+	public float health = 100;
+	[Export]
+	public float maxHealth = 100;
+	[Export]
+	public float attackDamage = 5f;
+	[Export]
+	public float attackCooldown = 0.6f;
 
-    public bool canAttack = true;
-    private bool rightFacing = true;
-    private bool canDash = false;
-
-
-
-    private Area2D hitBox;
-    private Area2D wallBoxL;
-    private Area2D wallBoxR;
-    private Timer attackCooldownTimer;
-    private Timer dashCoolTimer;
-    private Camera2D camera;
-    private AnimationNodeStateMachinePlayback animationPlayer;
-
-    public override void _Ready() {
-        base._Ready();
-        hitBox = GetNode<Area2D>("hitBox");
-        wallBoxL = GetNode<Area2D>("wallBoxL");
-        wallBoxR = GetNode<Area2D>("wallBoxR");
-        attackCooldownTimer = GetNode<Timer>("hitTimer");
-        dashCoolTimer = GetNode<Timer>("dashCoolTimer");
-        camera = GetNode<Camera2D>("camera");
-        animationPlayer = (AnimationNodeStateMachinePlayback)GetNode<AnimationTree>("AnimationTree").Get("parameters/playback");
-
-    }
-
-    public override void _PhysicsProcess(double delta) {
-        float dt = (float)delta;
-        Vector2 velocity = Velocity;
-
-        //Handle Attack 
-        if(Input.IsActionJustPressed("attack") && canAttack) {
-            animationPlayer.Travel("Attack");
-            Node2D closest = null;
-            foreach(Node2D node in hitBox.GetOverlappingBodies()) {
-                if(IsInstanceValid(node) && node is BaseEnemy) {
-                    if(
-                        closest == null ||
-                        (closest.GlobalPosition - GlobalPosition) > (node.GlobalPosition - GlobalPosition)
-                    ) {
-                        closest = node;
-                    }
-                }
-            }
-            if(closest != null)
-                (closest as BaseEnemy).damage(attackDamage);
-            addAttackCooldown();
-        }
+	public bool canAttack = true;
+	private bool rightFacing = true;
+	private bool canDash = false;
 
 
-        bool isPressingHorizontalKey = false;
-        // Get the input direction and handle the movement/deceleration.
-        float horizontalMovement = Input.GetAxis("move_left", "move_right");
-        if(horizontalMovement != 0) {
-            isPressingHorizontalKey = true;
-            setDirection(horizontalMovement > 0);
-            animationPlayer.Travel("Run");
-            if(IsOnFloor()) {
-                velocity.X += dt * horizontalMovement * speed;
-            } else {
-                if(horizontalMovement * speed < velocity.X || velocity.X < horizontalMovement * speed) {
-                    velocity.X += dt * horizontalMovement * airSpeed;
-                }
-            }
-        }
 
-        //Handle Dash
-        if(Input.IsActionJustPressed("dash") && canDash) {
-            velocity.X += rightFacing ? dashSpeed : -dashSpeed;
-            dashCoolTimer.Start(0.01f);
-            canDash = false;
-        }
+	private Area2D hitBox;
+	private Area2D wallBoxL;
+	private Area2D wallBoxR;
+	private Timer attackCooldownTimer;
+	private Timer dashCoolTimer;
+	private Camera2D camera;
+	private AnimationNodeStateMachinePlayback animationPlayer;
 
-        // Add the gravity and friction
-        if(IsOnFloor()) {
-            velocity.X *= 0.9f;
-        } else {
-            velocity.X *= 0.99f;
-            if(Input.IsActionPressed("fall"))
-                velocity += dt * GetGravity() * fallMulti * airPoundMulti;
-            velocity += dt * GetGravity() * fallMulti;
-        }
-        if(wallBoxR.HasOverlappingBodies()) {
-            if(velocity.X > 0) {
-                velocity.X = 0;
-            }
-        }
-        if(wallBoxL.HasOverlappingBodies()) {
-            if(velocity.X < 0) {
-                velocity.X = 0;
-            }
-        }
+	public override void _Ready() {
+		base._Ready();
+		hitBox = GetNode<Area2D>("hitBox");
+		wallBoxL = GetNode<Area2D>("wallBoxL");
+		wallBoxR = GetNode<Area2D>("wallBoxR");
+		attackCooldownTimer = GetNode<Timer>("hitTimer");
+		dashCoolTimer = GetNode<Timer>("dashCoolTimer");
+		camera = GetNode<Camera2D>("camera");
+		animationPlayer = (AnimationNodeStateMachinePlayback)GetNode<AnimationTree>("AnimationTree").Get("parameters/playback");
 
-        // Handle Jump.
-        if(Input.IsActionJustPressed("jump")) {
-            if(IsOnFloor()) {
-                animationPlayer.Travel("Jump");
-                velocity.Y = jumpVelocity;
-            } else if(wallBoxL.HasOverlappingBodies()) {
-                velocity.X = wallJumpVelocity;
-                velocity.Y = jumpVelocity;
-                if(!isPressingHorizontalKey)
-                    setDirection(true);
-            } else if(wallBoxR.HasOverlappingBodies()) {
-                velocity.X = -wallJumpVelocity;
-                velocity.Y = jumpVelocity;
-                if(!isPressingHorizontalKey)
-                    setDirection(false);
-            } else if(velocity.Y < 0) {
-                velocity.Y += jumpVelocity * 0.015f;
-            }
-        }
+	}
 
-        if(IsOnFloor())
-            canDash = true;
+	public override void _PhysicsProcess(double delta) {
+		float dt = (float)delta;
+		Vector2 velocity = Velocity;
 
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+		//Handle Attack 
+		if(Input.IsActionJustPressed("attack") && canAttack) {
+			animationPlayer.Travel("Attack");
+			Node2D closest = null;
+			foreach(Node2D node in hitBox.GetOverlappingBodies()) {
+				if(IsInstanceValid(node) && node is BaseEnemy) {
+					if(
+						closest == null ||
+						(closest.GlobalPosition - GlobalPosition) > (node.GlobalPosition - GlobalPosition)
+					) {
+						closest = node;
+					}
+				}
+			}
+			if(closest != null)
+				(closest as BaseEnemy).damage(attackDamage);
+			addAttackCooldown();
+		}
 
-    public void setDirection(bool right) {
-        float scale = right ? 1 : -1;
-        Scale = new Vector2(1f, scale);
-        RotationDegrees = right ? 0 : 180;
-        camera.Scale = new Vector2(scale, 1f);
-        if(right != (wallBoxL.Position.X < wallBoxR.Position.X))
-            (wallBoxL.Position, wallBoxR.Position) = (wallBoxR.Position, wallBoxL.Position);
-        rightFacing = right;
-    }
 
-    public void damage(float amount, Vector2 knockback = new Vector2()) {
-        GD.Print("hit: ", amount);
-        changeHealth(amount);
-        Velocity += knockback * 50;
-    }
+		bool isPressingHorizontalKey = false;
+		// Get the input direction and handle the movement/deceleration.
+		float horizontalMovement = Input.GetAxis("move_left", "move_right");
+		if(horizontalMovement != 0) {
+			isPressingHorizontalKey = true;
+			setDirection(horizontalMovement > 0);
+			animationPlayer.Travel("Run");
+			if(IsOnFloor()) {
+				velocity.X += dt * horizontalMovement * speed;
+			} else {
+				if(horizontalMovement * speed < velocity.X || velocity.X < horizontalMovement * speed) {
+					velocity.X += dt * horizontalMovement * airSpeed;
+				}
+			}
+		}
 
-    public virtual void changeHealth(float amount) {
-        health -= amount;
-        if(health <= 0.0f) {
-            kill();
-        }
-    }
+		//Handle Dash
+		if(Input.IsActionJustPressed("dash") && canDash) {
+			velocity.X += rightFacing ? dashSpeed : -dashSpeed;
+			dashCoolTimer.Start(0.01f);
+			canDash = false;
+		}
 
-    public virtual void kill() {
-        GD.Print("Dead");
-    }
+		// Add the gravity and friction
+		if(IsOnFloor()) {
+			velocity.X *= 0.9f;
+		} else {
+			velocity.X *= 0.99f;
+			if(Input.IsActionPressed("fall"))
+				velocity += dt * GetGravity() * fallMulti * airPoundMulti;
+			velocity += dt * GetGravity() * fallMulti;
+		}
+		if(wallBoxR.HasOverlappingBodies()) {
+			if(velocity.X > 0) {
+				velocity.X = 0;
+			}
+		}
+		if(wallBoxL.HasOverlappingBodies()) {
+			if(velocity.X < 0) {
+				velocity.X = 0;
+			}
+		}
 
-    public void addAttackCooldown(float amount = -1f) {
-        attackCooldownTimer.Start((double)amount);
-        canAttack = false;
-    }
+		// Handle Jump.
+		if(Input.IsActionJustPressed("jump")) {
+			if(IsOnFloor()) {
+				animationPlayer.Travel("Jump");
+				velocity.Y = jumpVelocity;
+			} else if(wallBoxL.HasOverlappingBodies()) {
+				velocity.X = wallJumpVelocity;
+				velocity.Y = jumpVelocity;
+				if(!isPressingHorizontalKey)
+					setDirection(true);
+			} else if(wallBoxR.HasOverlappingBodies()) {
+				velocity.X = -wallJumpVelocity;
+				velocity.Y = jumpVelocity;
+				if(!isPressingHorizontalKey)
+					setDirection(false);
+			} else if(velocity.Y < 0) {
+				velocity.Y += jumpVelocity * 0.015f;
+			}
+		}
 
-    public void resetAttackCooldown() {
-        canAttack = true;
-    }
-    public void resetDash() {
-        Velocity = new Vector2(Velocity.X > 0 ? speed * 0.6f : -speed * 0.6f, 0);
-    }
+		if(IsOnFloor())
+			canDash = true;
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+
+	public void setDirection(bool right) {
+		float scale = right ? 1 : -1;
+		Scale = new Vector2(1f, scale);
+		RotationDegrees = right ? 0 : 180;
+		camera.Scale = new Vector2(scale, 1f);
+		if(right != (wallBoxL.Position.X < wallBoxR.Position.X))
+			(wallBoxL.Position, wallBoxR.Position) = (wallBoxR.Position, wallBoxL.Position);
+		rightFacing = right;
+	}
+
+	public void damage(float amount, Vector2 knockback = new Vector2()) {
+		GD.Print("hit: ", amount);
+		changeHealth(amount);
+		Velocity += knockback * 50;
+	}
+
+	public virtual void changeHealth(float amount) {
+		health -= amount;
+		if(health <= 0.0f) {
+			kill();
+		}
+	}
+
+	public virtual void kill() {
+		GD.Print("Dead");
+	}
+
+	public void addAttackCooldown(float amount = -1f) {
+		attackCooldownTimer.Start((double)amount);
+		canAttack = false;
+	}
+
+	public void resetAttackCooldown() {
+		canAttack = true;
+	}
+	public void resetDash() {
+		Velocity = new Vector2(Velocity.X > 0 ? speed * 0.6f : -speed * 0.6f, 0);
+	}
 }
--- 2024rpijam/scripts/UI/LevelSelect.cs
+++ 2024rpijam/scripts/UI/LevelSelect.cs
@@ -1,20 +1,38 @@
 using Godot;
 using System;
+//using Collections;
 
 public partial class LevelSelect : Control
 {
 	[Export]
 	public int highestLevel = 1;
+	[Export]
+	public int levelCount = 4;
+	public Godot.Collections.Array<string> buttons = new Godot.Collections.Array<string>{
+		"level1",
+		"level2",
+		"level3",
+		"level4"
+	};
 	
 	public override void _Ready(){
 		GD.Print("Start");
-		base._Ready();
-		foreach(Control child in GetChildren()){
+		for(int i = 1; i < buttons.Count + 1; i++){
+			string getter = "Level" + i;
+			Button button = GetNode<Button>(getter);
+			if(i > highestLevel)
+				button.Disabled = true;
+			else
+				button.Disabled = false;
+		}
+		GD.Print("End");
+		/*base._Ready();
+		foreach(var child in GetChildren()){
 			GD.Print("Child: " + child);
 			if(IsInstanceValid(child) && child is BaseButton button){
 				GD.Print("found");
 			}
 		}
-		GD.Print("End");
+		*/
 	}
 }
